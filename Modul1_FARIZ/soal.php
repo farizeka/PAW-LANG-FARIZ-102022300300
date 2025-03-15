57c23dc453e044064b37cfb3ec5fcf21381926fd
Eka <97788181+farizeka@users.noreply.github.com>
2025-03-16 00:33:47 +0700

    praktikum modul1

--- Modul1_FARIZ/soal.php
+++ Modul1_FARIZ/soal.php
@@ -1,34 +1,70 @@
 <?php
+// **********************  1  **************************  
 // Inisialisasi variabel untuk menyimpan nilai input dan error
-$nama = $email = $nim = "";
-$namaErr = $emailErr = $nimErr = "";
+$nama = $email = $nim = $jurusan = $fakultas = "";
+$namaErr = $emailErr = $nimErr = $jurusanErr = $fakultasErr =  "";
 
-    // **********************  1  **************************  
-    // Tangkap nilai nama yang ada pada form HTML
-    // silakan taruh kode kalian di bawah
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["nama"])) {
-        $namaErr = "Nama wajib diisi";
-    } 
-
     // **********************  2  **************************  
-    // Validasi format email agar sesuai dengan standar
+    // - Tangkap nilai nama yang ada pada form HTML (Lihat Task 7)
+    // - Validasi agar nama tidak boleh kosong
+    // - Validasi agar nama hanya berupa abjad (Hint : gunakan fungsi preg_match (atau fungsi lainnya))
     // silakan taruh kode kalian di bawah
-    if (empty($_POST["email"])) {
-        $emailErr = "Email wajib diisi";
-    } 
+    $nama = $_POST["nama"];
+    if (empty($nama)) {
+        $namaErr = "nama wajib diisi";
+    }
 
     // **********************  3  **************************  
-    // Pastikan NIM terisi dan  angka
+    // - Tangkap nilai email yang ada pada form HTML (Lihat Task 7)
+    // - Memeriksa apakah email kosong
+    // - Memeriksa apakah format email valid (Hint : gunakan fungsi filter_var)
     // silakan taruh kode kalian di bawah
-    if (empty($_POST["nim"])) {
+    $email = $_POST["email"];
+    if (empty($email)) {
+        $emailErr = "Email wajib diisi";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Email harus valid";
+    }
+ 
+
+    // **********************  4  **************************  
+    // - Tangkap nilai NIM yang ada pada form HTML (Lihat Task 7)
+    // - Pastikan NIM terisi dan berformat angka
+    // - silakan taruh kode kalian di bawah
+    $nim = $_POST["nim"];
+    if (empty($nim)) {
         $nimErr = "NIM wajib diisi";
     } elseif (!ctype_digit($nim)) {
         $nomorErr = "NIM harus berupa angka";
     }
+
+
+    // **********************  5  **************************  
+    // - Tangkap nilai jurusan yang ada pada form HTML (Lihat Task 7)
+    // - Validasi agar jurusan tidak boleh kosong
+    // - Validasi agar jurusan hanya berupa abjad (Hint : gunakan fungsi preg_match (atau fungsi lainnya))
+    // silakan taruh kode kalian di bawah
+    $jurusan = $_POST["jurusan"];
+    if (empty($jurusan)) {
+        $jurusanErr = "Jurusan wajib diisi";
+    } elseif (!preg_match('/^[A-Za-z\s]+$/', $jurusan)) {
+        $jurusanErr = "Jurusan hanya abjad";
+    }
+ 
+
+    // **********************  6  **************************  
+    // - Tangkap nilai fakultas yang ada pada form HTML (Lihat Task 7)
+    // - Validasi agar fakultas tidak boleh kosong
+    // - Validasi agar fakultas hanya berupa abjad (Hint : gunakan fungsi preg_match (atau fungsi lainnya))
+    // silakan taruh kode kalian di bawah
+    $fakultas = $_POST["fakultas"];
+    if (empty($fakultas)) {
+        $fakultasErr = "Fakultas wajib diisi";
+    } elseif (!preg_match('/^[A-Za-z\s]+$/', $fakultas)) {
+        $fakultasErr = "Fakultas hanya abjad";
+    }
 }
-   
-    
 ?>
 
 <!DOCTYPE html>
@@ -45,53 +81,90 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h2>Formulir Pendaftaran Mahasiswa Baru</h2>
 
         <?php if ($_SERVER["REQUEST_METHOD"] == "POST") { ?>
-            <?php if (!empty($namaErr) || !empty($emailErr) || !empty($nimErr)) { ?>
-                <div class="alert alert-danger">
-                    <strong>Kesalahan!</strong> Harap perbaiki data yang salah.
-                </div>
+            <?php if (!empty($namaErr) || !empty($emailErr) || !empty($nimErr) || !empty($jurusanErr) || !empty($fakultasErr)) { ?>
+            <div class="alert alert-danger">
+                <strong>Kesalahan!</strong> Harap perbaiki data yang salah.
+            </div>
             <?php } else { ?>
-                <div class="alert alert-success">
-                    <strong>Berhasil!</strong> Data pendaftaran telah diterima.
-                </div>
+            <div class="alert alert-success">
+                <strong>Berhasil!</strong> Data pendaftaran telah diterima.
+            </div>
             <?php } ?>
         <?php } ?>
 
         <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-
-         <!-- **********************  4  ************************** -->
-         <!-- Tambahkan kolom input untuk Nama, Email, dan NIM dengan mengambil class form-group sebagai referensi -->
-
-         <div class="form-group">
-                <!-- tambahkan label nama -->
-                <label for="nama">Nama</label>
-                <input type="text" id="nama" name="nama" value="<?php echo $nama; ?>">
-                <!-- Tampilkan pesan error jika variabel $namaErr tidak kosong -->
-                <span class="error"><?php echo $namaErr ? "* $namaErr" : ""; ?></span>
+            <!-- **********************  7  ************************** -->
+            <!-- Tambahkan value di tiap form-group/kolom untuk menampilkan kembali data di form setelah submit (retaining input) -->
+            <!-- Hint : value pada input form harus berisi variabel yang menyimpan data input -->
+            <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" id="nama" name="nama" value="<?php echo $nama; ?>">
+            <span class="error"><?php echo $namaErr ? "* $namaErr" : ""; ?></span>
             </div>
 
             <div class="form-group">
-                <!-- tambahkan label email -->
-                <label for="email">E-mail</label>
-                <input type="text" id="email" name="email" value="<?php echo $email; ?>">
-                <!-- Tampilkan pesan error jika variabel $emailErr tidak kosong -->
-                <span class="error"><?php echo $emailErr ? "* $emailErr" : ""; ?></span>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" value="<?php echo $email;?>">
+            <span class="error"><?php echo $emailErr ? "* $emailErr" : ""; ?></span>
             </div>
 
             <div class="form-group">
-                <!-- tambahkan label nim -->
-                <label for="nim">NIM</label>
-                <input type="text" id="nim" name="nim" value="<?php echo $nim; ?>">
-                <!-- Tampilkan pesan error jika variabel $nimErr tidak kosong -->
-                <span class="error"><?php echo $nimErr ? "* $nimErr" : ""; ?></span>
+            <label for="nim">NIM</label>
+            <input type="text" id="nim" name="nim" value="<?php echo $nim; ?>" >
+            <span class="error"><?php echo $nimErr ? "* $nimErr" : ""; ?></span>
+            </div>
+
+            <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <input type="text" id="jurusan" name="jurusan" value="<?php echo $jurusan;?>">
+            <span class="error"><?php echo $jurusanErr ? "* $jurusanErr" : ""; ?></span>
+            </div>
+
+            <div class="form-group">
+            <label for="fakultas">Fakultas</label>
+            <input type="text" id="fakultas" name="fakultas" value="<?php echo $fakultas;?>" >
+            <span class="error"><?php echo $fakultasErr ? "* $fakultasErr" : ""; ?></span>
             </div>
 
             <div class="button-container">
-                <button type="submit">Daftar</button>
+            <button type="submit">Daftar</button>
             </div>
-        
-
         </form>
     </div>
+
+    <!-- **********************  8  ************************** -->
+    <!-- Panggil variabel yang berisi pesan error (Hint : gunakan if dan metode post) -->
+    <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && !$namaErr && !$emailErr && !$nimErr && !$jurusanErr && !$fakultasErr) { ?>
+
+    <div class="container">
+        <h3>Data Pendaftaran</h3>
+        <div class="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th width="20%">Nama</th>
+                        <th width="20%">Email</th>
+                        <th width="20%">NIM</th>
+                        <th width="20%">Jurusan</th>
+                        <th width="20%">Fakultas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $nama; ?></td>
+                        <td><?php echo $email; ?></td>
+                        <td><?php echo $nim; ?></td>
+                        <td><?php echo $jurusan; ?></td>
+                        <td><?php echo $fakultas; ?></td>
+                    </tr>
+                </tbody>
+            </table>
+    <!-- **********************  9  ************************** -->
+    <!-- Tampilkan data pendaftaran dalam bentuk tabel yang baru saja diinput -->
+     
+        </div>
+    </div>
+    <?php } ?>
 </body>
 </html>
 
